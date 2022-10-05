ba6cc6deb373501756a22a86b43b07254fa4be8f
charlesMuchogo <muchpaul2@gmail.com>
2022-10-05 21:59:55 +0300

    added check for remaining tickets

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ func main()  {
 	var firstName string
 	var lastName string
 	var totalTickets uint = 50
-	var remainingTickets uint
+	var remainingTickets uint = totalTickets
 	var bookedTickets uint
+
+	 names := []string{} 
+	
 	
 
 	fmt.Println("XXXXXXXXXXXXXXX Welcome to concert ticket booking system XXXXXXXXXXXXXXX")
+
+	for {
+
+	
 
 	fmt.Println(firstName, "Enter your first name: ")
 	fmt.Scanln(&firstName)
@@ -21,11 +28,25 @@ func main()  {
 	fmt.Println(firstName, "Enter your last name: ")
 	fmt.Scanln(&lastName)
 
+	
+
 	fmt.Println("Enter the number of tickets you want to book: ")
 	fmt.Scanln(&bookedTickets)
 
-	remainingTickets = totalTickets - bookedTickets
+	
+
+	if bookedTickets > totalTickets {
+		fmt.Printf("Sorry. we only have %v tickets left \n", remainingTickets )
+		continue;
+	}
+
+	remainingTickets = remainingTickets - bookedTickets
+	names = append(names, firstName +" "+ lastName)
 	fmt.Printf("Thank you %v %v, You have booked %v tickets. Total remaining tickets are %v \n", firstName, lastName, bookedTickets, remainingTickets)
+	fmt.Println(names)
+	}
+
+	
 	
 }
 
